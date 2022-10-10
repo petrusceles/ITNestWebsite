c163e75e928b9c89edb58dc88d9ff353d810e45e
petrusceles <petrusprihantoro@gmail.com>
2022-10-10 16:51:29 +0700

    initial

---
+++ components/layout.js
@@ -0,0 +1,34 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from './layout.module.css';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+
+const name = 'Celes';
+export const siteTitle = 'IT NEST';
+
+export default function Layout({ children}) {
+  return (
+    <div className="container">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle,
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      {/* <header>
+
+      </header> */}
+      <main>{children}</main>
+    </div>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+import Date from '../components/date';
+
+import { getSortedPostsData } from '../lib/posts';
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({allPostsData}) {
+  return (
+    <Layout qweqeqe>
+    </Layout>
+  );
+}
