eb790e0e36cd766b5da7cd31ed1a80a21a70d9ba
petrusceles <petrusprihantoro@gmail.com>
2022-10-12 20:51:43 +0700

    fix bug for navbar

--- components/navbar.js
+++ components/navbar.js
@@ -5,10 +5,10 @@ import { useRef } from 'react'
 export default function Navbar () {
     const offCanvas = useRef(null)
     const hideOffCanvas = () => {
-        offCanvas.current.classList.remove('translate-x-[0%]')
+        offCanvas.current.classList.replace('translate-x-[0%]','translate-x-[100%]')
     }
     const showOffCanvas = () => {
-        offCanvas.current.classList.add('translate-x-[0%]')
+        offCanvas.current.classList.replace('translate-x-[100%]','translate-x-[0%]')
     }
     const list = ["Home", "Solution", "Why Us", "Contact"]
     return (
@@ -27,14 +27,14 @@ export default function Navbar () {
                     </Link>
                 </div>
                 <div className='flex w-3/4 justify-center'>
-                    <button id="hamburger" onClick={showOffCanvas} name="hamburger" type="button" class="block group absolute bottom-1/4 pl-4 pr-1 right-[8%] top-1/4 lg:hidden">
+                    <button id="hamburger" onClick={showOffCanvas} name="hamburger" type="button" className="block group absolute bottom-1/4 pl-4 pr-1 right-[8%] top-1/4 lg:hidden">
                         <HamburgerButton />
                     </button>
                     <div ref={offCanvas} className='flex-wrap bg-gradient-to-r from-violet-100 to-cyan-50 px-8 sm:px-16 translate-x-[100%] transition-all duration-300 ease-in-out md:px-20 pt-9 right-0 top-0 h-[1000px] w-1/2 z-10 absolute lg:translate-x-0 lg:static lg:h-full lg:w-full lg:flex lg:gap-20 lg:content-center lg:pt-0 lg:justify-end lg:bg-gradient-to-r lg:from-transparent lg:to-transparent'>
                         <div className='flex justify-between w-full lg:hidden'>
-                            <h2 class="font-semibold text-xl text-primary">IT NEST</h2>
+                            <h2 className="font-semibold text-xl text-primary">IT NEST</h2>
                             <button id="close" onClick={hideOffCanvas}>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16" transform="scale(3.0)">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16" transform="scale(3.0)">
                                     <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
                                 </svg>
                             </button>
@@ -52,9 +52,9 @@ export default function Navbar () {
 }
 
 function HamburgerButton({}) {
-    return (<><span class="hamburger-line group-hover:-translate-x-1  transition-all duration-100 ease-in-out "></span>
-                      <span class="hamburger-line"></span>
-                      <span class="hamburger-line group-hover:-translate-x-1  transition-all duration-100 ease-in-out "></span></>);
+    return (<><span className="hamburger-line group-hover:-translate-x-1  transition-all duration-100 ease-in-out "></span>
+                      <span className="hamburger-line"></span>
+                      <span className="hamburger-line group-hover:-translate-x-1  transition-all duration-100 ease-in-out "></span></>);
 }
 
 
