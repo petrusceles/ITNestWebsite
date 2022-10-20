57e9a021c5b5a274ee88e4736a7507c83cd3fa25
petrusceles <petrusprihantoro@gmail.com>
2022-10-20 10:22:21 +0700

    class to classname

--- components/navbar.js
+++ components/navbar.js
@@ -79,7 +79,7 @@ function NavList({list}) {
                     return (
                     <>
                     <SolutionDropDown className={"overflow-hidden opacity-0 h-fit group-hover:opacity-100 hidden p-2 lg:block absolute flex-wrap w-[200%] top-[150%] bg-violet-50/90 rounded-md transition-all duration-300 ease-in-out shadow-md"}/>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill absolute -right-3 hidden lg:block" viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-down-fill absolute -right-3 hidden lg:block" viewBox="0 0 16 16">
                         <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
                     </svg>
                     </>
