759b114b2446dddf6fac0668e21017eeb44bacb8
petrusceles <petrusprihantoro@gmail.com>
2022-10-22 03:22:18 +0700

    delete button learn more and added new objects

--- components/solution.js
+++ components/solution.js
@@ -2,17 +2,21 @@ import SolutionSegment from './solutionSegment'
 import AIImage from '../public/images/solutions/AISolution.png'
 import IoTImage from '../public/images/solutions/IoTSolution.png'
 import SoftwareImage from '../public/images/solutions/SoftwareSolution.png'
+import AIIcon from '../public/images/solutionIcons/ai.png'
+import IoTIcon from '../public/images/solutionIcons/iot.png'
+import SoftwareIcon from '../public/images/solutionIcons/software.png'
 import SolutionContent from '../posts/solutionContents.json' assert {type: 'json'};
 export default function Solution () {
     const solutions = SolutionContent.solution;
     const images = [AIImage, IoTImage, SoftwareImage]
+    const icon = [AIIcon,IoTIcon, SoftwareIcon]
     const solutionsReadyToUse = solutions.map((e,i) => {
         return (
-            <SolutionSegment key={i} title={e.title} image={images[i]} content={e.content} tagline={e.tagline} id={e.id}/>)
+            <SolutionSegment key={i} title={e.title} image={images[i]} content={e.content} tagline={e.tagline} id={e.id} icon={icon[i]}/>)
     })
     // console.log(solutionsReadyToUse)
     return (
-        <div id='solution' className='mt-10 anchor'>
+        <div id='solution' className='mt-28 anchor'>
             <div className='flex items-center gap-6'>
                 <div className='flex w-fit items-center'>
                     <h1 className='  font-semibold content-center text-3xl lg:text-4xl'>SOLUTIONS</h1>
